6355e23a160c4dc5c03830c6d0d9e6e82f875e07
Dylan Harness <dharness.engineer@gmail.com>
2023-07-23 20:16:38 -0700

    cors

--- server/yogoflow/app.py
+++ server/yogoflow/app.py
@@ -4,16 +4,22 @@ load_dotenv()
 from yogoflow.services.pose_model import pose_model
 from yogoflow.routes.video import VideoApi
 from flask import Flask
-from flask_restful import Api
+from flask_restful import Api, Resource
 from flask_cors import CORS
+
+
+class Health(Resource):
+  def get(self):
+    return 200
 
 
 def create_app():
 
   app = Flask(__name__)
-  api = Api(app)
   CORS(app)
+  api = Api(app)
 
+  api.add_resource(Health, '/health')
   api.add_resource(VideoApi, '/api/video')
 
   return app
