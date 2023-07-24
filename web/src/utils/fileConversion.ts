c2b2a85c8ef2a6a358faa8e988ad7174e4c2a3b3
Dylan Harness <dharness.engineer@gmail.com>
2023-07-23 22:03:44 -0700

    .

--- web/src/utils/fileConversion.ts
+++ web/src/utils/fileConversion.ts
@@ -1,7 +1,5 @@
 /**
  * This function converts a blob url to a File object.
- * @param blobUrl
- * @returns
  */
 export const blobUrlToFile = async (blobUrl: string): Promise<File> => {
   const response = await fetch(blobUrl);
